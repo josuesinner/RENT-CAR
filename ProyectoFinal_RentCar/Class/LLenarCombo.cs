bdaa9c5c15c45c9becd0b289a1fd6bc8959e2f65
Josue Correa <josue03correa@hotmail.com>
2022-05-21 12:12:40 -0400

    agregando clase para llenar combos

---
+++ ProyectoFinal_RentCar/Class/LLenarCombo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_RentCar.Class
+{
+    public class LLenarCombo
+    {
+        public List<Marcas> ComboMarca()
+        {
+            using (BD_Context db = new BD_Context())
+            {
+                return db.Marcas.ToList();
+            }
+        }
+
+        public List<Modelos> ComboModelo(int pId)
+        {
+            using (BD_Context db = new BD_Context())
+            {
+                return db.Modelos.Where(m=>m.MarcaId==pId).ToList();
+            }
+        }
+    }
+}
